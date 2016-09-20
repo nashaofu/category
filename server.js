939eebe214d0f76320a2df48230808da03f25326
unknown <>
2016-09-20 23:51:08 +0800

    支持动态修改插件配置，完善示例

--- server.js
+++ server.js
@@ -1,22 +1,53 @@
+/**
+ * 创建一个简单的静态文件服务器
+ */
 'use strict';
+// http模块
 const http = require('http');
-const fs = require('fs');
+// url模块
 const url = require('url');
+// 路径模块
 const path = require('path');
-const port = 8080;
+// 文件系统模块
+const fs = require('fs');
+// 服务器配置
+let CONFIG = {
+    // 服务器端口
+    port: 8080,
+    // 网站根目录
+    root: __dirname,
+    // 默认文档
+    filename: 'index.html'
+};
+// 创建http服务器
 let server = http.createServer(function(req, res) {
-    let pathName = url.parse(req.url).pathname;
-    let filename = path.join(__dirname, pathName);
-    if (fs.existsSync(filename)) {
-        filename = fs.statSync(filename).isFile() ? filename : filename + 'index.html';
-        res.end(fs.readFileSync(filename));
-    } else {
-        res.writeHead(400, {
-            "Content-Type": "text/plain"
-        });
-        res.end("404 NOT FOUND");
-        console.log(pathName + "  " + "404");
-    }
+    // 路径名
+    let pathname = url.parse(req.url).pathname;
+    // 文件名
+    let filename = path.join(CONFIG.root, pathname);
+    // 异步获取文件信息
+    fs.stat(filename, function(err, stats) {
+        // 定义状态码
+        let status = null;
+        if (!err) {
+            // 判断是否为文件夹
+            filename = stats.isDirectory() ? filename + CONFIG.filename : filename;
+            // 状态码设置为200
+            status = 200;
+            // 返回数据
+            res.writeHead(status);
+            fs.createReadStream(filename).pipe(res);
+        } else {
+            // 状态码设置为404
+            status = 404;
+            // 返回数据
+            res.writeHead(status);
+            res.end('404 Not Found');
+        }
+        // 打印日志
+        console.log(status +'  '+ req.url);
+    });
 });
-server.listen(port);
-console.log('Server running at port:' + port);
+// 监听端口
+server.listen(CONFIG.port);
+console.log('Server is running at http://127.0.0.1:' + CONFIG.port + '/');
--- src/category.js
+++ src/category.js
@@ -33,7 +33,7 @@
     };
     Category.DEFAULTS = {
         name: "分类",
-        key: "key",
+        key: null,
         url: undefined,
         formdata: null,
         all: {
@@ -44,9 +44,10 @@
     Category.prototype = {
         init: function() {
             var me = this;
-            me.onClick();
+            me.bindEvent();
+            // 优先加载本地数据
             if (typeof me.options.data == "object") {
-                me.render(me.options.data);
+                me.render();
             } else if (typeof me.options.url == "string") {
                 me.getData();
             }
@@ -55,7 +56,7 @@
             var me = this;
             me.options = $.extend({}, me.options, typeof options == "object" && options);
             if (typeof me.options.url == "undefined") {
-                console.warn("未定义url");
+                throw new Error("未定义url");
                 return false;
             }
             $.ajax({
@@ -64,7 +65,9 @@
                 data: me.options.formdata,
                 dataType: "json",
                 success: function(data) {
-                    me.render(data);
+                    me.render({
+                        data: data
+                    });
                 },
                 error: function(error) {
                     try {
@@ -73,12 +76,13 @@
                 }
             });
         },
-        render: function(data) {
-            var me = this,
-                // 数据值
-                data = data[me.options.key] ? data[me.options.key] : data,
-                // 条目名称
-                $name = $('<li></li>').text(me.options.name + ':'),
+        render: function(options) {
+            var me = this;
+            me.options = $.extend({}, me.options, typeof options == "object" && options);
+            // 获取数据
+            me.options.data = me.options.key && me.options.data[me.options.key] ? me.options.data[me.options.key] : me.options.data;
+            // 条目名称
+            var $name = $('<li></li>').text(me.options.name + ':'),
                 // 全部标签
                 $all = $('<li class="active"><a href="javascript:void(0)">' + me.options.all.name + '</a></li>');
             $all.data(me.options.key, me.options.all.value);
@@ -86,14 +90,14 @@
             me.$target.empty().append($name).append($all).data("key", me.options.all.value);
             // 存储生成的标签
             var $categorys = [];
-            $.each(data, function(i) {
-                var $category = $('<li><a href="javascript:void(0)">' + data[i] + '</a></li>');
-                $category.data(me.options.key, data[i]);
+            $.each(me.options.data, function(i, item) {
+                var $category = $('<li><a href="javascript:void(0)">' + item + '</a></li>');
+                $category.data(me.options.key, item);
                 $categorys.push($category);
             });
             me.$target.append($categorys);
         },
-        onClick: function() {
+        bindEvent: function() {
             var me = this;
             me.$target.on("click.category", "li>a", function() {
                 var $category = $(this).parent("li");
@@ -104,7 +108,7 @@
     };
     // 外部可调用API
     Category.API = ["getData", "render"];
-    $.fn.category = function(options, data) {
+    $.fn.category = function(options, parameter) {
         if (this.length <= 0) {
             return this;
         }
@@ -112,7 +116,7 @@
             if (typeof options == "string" && $.inArray(options, Category.API) !== -1) {
                 var API = options;
                 var category = this.data("category");
-                category[API](data);
+                category[API](parameter);
             }
         } else {
             this.data("category", new Category(this, options));
